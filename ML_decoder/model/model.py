633356570a65d3ff937ec899bcc603abe1cb7484
HeoYB <tiger_ak@doinglab.com>
2022-11-10 08:34:03 +0900

    Fix backborn

--- ML_decoder/model/model.py
+++ ML_decoder/model/model.py
@@ -8,28 +8,25 @@ class MLDcoderClassification(nn.Module):
     def __init__(self, n_level_1, n_level_2, n_level_3):
         super(MLDcoderClassification, self).__init__()
         self.backborn = timm.create_model(model_name=CFG.MODEL_NAME, pretrained=True, num_classes=0, drop_rate=0.3)
-        self.backborn_non_gap = nn.Sequential(*(list(self.backborn.children())[:-2]))
+        self.backborn_non_gap = nn.Sequential(*(list(self.backborn.children())[:-1]))
         
         self.level_1_decoder = nn.Sequential(
-            nn.Identity(),
             MLDecoder(num_classes=n_level_1,
-                      initial_num_features=1280,
+                      initial_num_features=2048,
                       num_of_groups=1,
                       decoder_embedding=768,
                       zsl=0)
         )
         self.level_2_decoder = nn.Sequential(
-            nn.Identity(),
             MLDecoder(num_classes=n_level_2,
-                      initial_num_features=1280,
+                      initial_num_features=2048,
                       num_of_groups=1,
                       decoder_embedding=768,
                       zsl=0)
         )
         self.level_3_decoder = nn.Sequential(
-            nn.Identity(),
             MLDecoder(num_classes=n_level_3,
-                      initial_num_features=1280,
+                      initial_num_features=2048,
                       num_of_groups=1,
                       decoder_embedding=768,
                       zsl=0)
